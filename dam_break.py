b8e76c9fd6ef56486744df8de789c2d009df49fa
amalss18 <amalssebastian@gmail.com>
2020-01-03 22:41:20 +0530

    debugging

--- dam_break.py
+++ dam_break.py
@@ -1,9 +1,9 @@
 import numpy as np
 from mayavi import mlab
-from numba import njit
+from numba import njit, jit
 from numpy.linalg  import norm
 
-# @njit
+@jit
 def cubic_spline_2d(xij, h):
 	rij = norm(xij)
 	q = rij/h
@@ -13,22 +13,23 @@ def cubic_spline_2d(xij, h):
 	elif q<=2.0:
 		return 0.25*a*(2.0-q)**3.0
 	else:
-		return 0.0
+		return np.array([0.0, 0.0])
 
-# @njit
+@jit
 def cubic_spline_derivative_2d(xij,h):
 	rij = norm(xij)
 	q = rij/h
 	a = float(10.0/(7.0*np.pi*(h**2)))
 	if q<=1.0 and q>=0.0:
-		if not xij ==0.0 :
-		    return float(a*((-3.0*q)+ 2.25*(q**2))*(xij/rij)/h)
+		if not rij == 0.0 :
+			return (a*((-3.0*q)+ 2.25*(q**2))*(xij/rij)/h)
 		else :
-		    return 0.0
+			return np.array([0.0, 0.0])
 	elif q<=2.0:
-		    return float((-0.75*a*(2-q)**2)*(xij/rij)/h)
+		return ((-0.75*a*(2-q)**2)*(xij/rij)/h)
 	elif q>2.0:
-		return 0.0
+		return np.array([0.0, 0.0])
+
 
 
 class DamBreak2D():
@@ -54,15 +55,16 @@ class DamBreak2D():
         self.fx1 = np.vstack((self.fx,self.fy)).T
 
         self.solid = {"m": np.ones_like(self.sx),"p" : np.zeros_like(self.sx),"rho" : np.ones_like(self.sx)*self.rho0,
-                 "x": self.sx1, "v": np.zeros((len(self.sx),2)), "h": np.ones_like(self.sx)*1.2*self.dx}
+                    "x": self.sx1, "v": np.zeros((len(self.sx),2)), "h": np.ones_like(self.sx)*1.2*self.dx}
 
         self.fluid = {"m": np.ones_like(self.fx),"p" : np.zeros_like(self.fx),"rho" : np.ones_like(self.fx)*self.rho0,
-                 "x": self.fx1, "v": np.zeros((len(self.fx),2)), "h": np.ones_like(self.sx)*1.2*self.dx}
+                    "x": self.fx1, "v": np.zeros((len(self.fx),2)), "h": np.ones_like(self.sx)*1.2*self.dx}
 
         self.x = np.concatenate((self.sx1,self.fx1))
+        le = self.x.shape[0]
 
-        self.sf = {"m": np.ones_like(self.x),"p" : np.zeros_like(self.x),"rho" : np.ones_like(self.x)*(self.dx**2),
-                 "x": self.x, "v": np.zeros_like(self.x), "h": np.ones_like(self.x)*1.2*self.dx}
+        self.sf = {"m": np.ones(le),"p" : np.zeros(le),"rho" : np.ones(le)*(self.dx**2),
+                    "x": self.x, "v": np.zeros_like(self.x), "h": np.ones(le)*1.2*self.dx}
 
 
     def create_particles(self):
@@ -92,23 +94,22 @@ class DamBreak2D():
 
         return solid_x, solid_y, fluid_x, fluid_y
 
-    # @njit(parallel=True)
+
     def solid_calc(self,solid,sf):
         arho_solid = np.zeros_like(solid["m"])
         for i in range(len(solid["m"])):
             sum_rho = 0
 
-            for j in range(sf["m"]):
+            for j in range(len(sf["m"])):
                 xij = solid["x"][i] - sf["x"][j]
                 vij = solid["v"][i] - sf["v"][j]
 
-                sum_rho += (sf["m"][j]*vij*cubic_spline_2d(xij,solid["h"][i]))/sf["rho"][j]
+                sum_rho += (sf["m"][j]*np.dot(vij,cubic_spline_derivative_2d(xij,solid["h"][i])))/sf["rho"][j]
 
             arho_solid[i] = solid["rho"][i] * sum_rho
 
         return arho_solid
 
-    # @njit(parallel=True)
     def fluid_calc(self,fluid, sf):
         arho_fluid = np.zeros_like(fluid["m"])
         acc_fluid = np.zeros_like(fluid["x"])
@@ -129,11 +130,12 @@ class DamBreak2D():
                 pi_ij = -self.aplha * self.c0 * muij / rhoij
 
                 pj_rhoj = sf["p"][j] / sf["rho"][j]**2
-                pi_rhoi = sf["p"][i] / sf["rho"][i]**2
+                pi_rhoi = fluid["p"][i] / fluid["rho"][i]**2
 
-                sum_rho += (sf["m"][j]*vij*cubic_spline_2d(xij,fluid["h"][i]))/sf["rho"][j]
+                sum_rho += sf["m"][j]*np.dot(vij,cubic_spline_derivative_2d(xij,fluid["h"][i]))/sf["rho"][j]
                 sum_vel += sf["m"][j]*(pi_rhoi+pj_rhoj+pi_ij)*cubic_spline_derivative_2d(xij,fluid["h"][i]) - np.array([0,self.g])
                 sum_x += sf["m"][j]*vij*cubic_spline_2d(xij,fluid["h"][i])
+
 
             arho_fluid[i] = fluid["rho"][i] * sum_rho
             acc_fluid[i] = -1 * sum_vel
@@ -144,67 +146,62 @@ class DamBreak2D():
 
     def update_values(self,tf,dt):
 
-        solid = self.solid
-        fluid = self.fluid
-        sf = self.sf
-
         arho_solid = np.zeros_like(self.solid["m"])
         B = (self.c0**2)*self.rho0/self.gamma
-        arho_fluid = np.zeros_like(fluid["m"])
-        acc_fluid = np.zeros_like(fluid["x"])
-        dxdt_fluid = np.zeros_like(fluid["x"])
+        arho_fluid = np.zeros_like(self.fluid["m"])
+        acc_fluid = np.zeros_like(self.fluid["x"])
+        dxdt_fluid = np.zeros_like(self.fluid["x"])
 
         # for solid particles
-        for i in range(len(solid["m"])):
-            sum_rho = 0
+        # for i in range(len(solid["m"])):
+        #     sum_rho = 0
 
-            for j in range(len(sf["m"])):
-                xij = solid["x"][i] - sf["x"][j]
-                vij = solid["v"][i] - sf["v"][j]
+        #     for j in range(len(sf["m"])):
+        #         xij = solid["x"][i] - sf["x"][j]
+        #         vij = solid["v"][i] - sf["v"][j]
 
-                sum_rho += (sf["m"][j]*np.dot(vij,cubic_spline_2d(xij,solid["h"][i])))/sf["rho"][j]
+        #         sum_rho += (sf["m"][j]*np.dot(vij,cubic_spline_derivative_2d(xij,solid["h"][i])))/sf["rho"][j]
 
-            print(sum_rho)
-            arho_solid[i] = solid["rho"][i] * sum_rho
+        #     arho_solid[i] = solid["rho"][i] * sum_rho
 
-        # arho_solid = self.solid_calc(solid, sf)
+        arho_solid = self.solid_calc(self.solid, self.sf)
 
         self.solid["rho"] += dt*arho_solid
-        self.solid["p"] = B*((solid["rho"]/self.rho0)**(self.gamma) - 1)
+        self.solid["p"] = B*((self.solid["rho"]/self.rho0)**(self.gamma) - 1)
 
         # for fluid particles
 
-        for i in range(len(fluid["x"])):
-            sum_rho = 0
-            sum_vel = 0
-            sum_x = 0
+        # for i in range(len(fluid["x"])):
+        #     sum_rho = 0
+        #     sum_vel = 0
+        #     sum_x = 0
+        #     print(len(fluid["x"]),len(sf["x"]))
+        #     for j in range(len(sf["x"])):
+        #         xij = fluid["x"][i] - sf["x"][j]
+        #         rij = norm(xij)
+        #         vij = fluid["v"][i] - sf["v"][j]
+        #         rhoij = 0.5*(fluid["rho"][i]+sf["rho"][j])
 
-            for j in range(len(sf["x"])):
-                xij = fluid["x"][i] - sf["x"][j]
-                rij = norm(xij)
-                vij = fluid["v"][i] - sf["v"][j]
-                rhoij = 0.5*(fluid["rho"][i]+sf["rho"][j])
+        #         muij = fluid["h"][i] * np.dot(vij,xij) / (rij+self.eta**2)
+        #         pi_ij = -self.aplha * self.c0 * muij / rhoij
 
-                muij = fluid["h"][i] * np.dot(vij,xij) / (rij+self.eta**2)
-                pi_ij = -self.aplha * self.c0 * muij / rhoij
+        #         pj_rhoj = sf["p"][j] / sf["rho"][j]**2
+        #         pi_rhoi = sf["p"][i] / sf["rho"][i]**2
 
-                pj_rhoj = sf["p"][j] / sf["rho"][j]**2
-                pi_rhoi = sf["p"][i] / sf["rho"][i]**2
+        #         sum_rho += sf["m"][j]*np.dot(vij,cubic_spline_derivative_2d(xij,fluid["h"][i]))/sf["rho"][j]
+        #         sum_vel += sf["m"][j]*(pi_rhoi+pj_rhoj+pi_ij)*cubic_spline_derivative_2d(xij,fluid["h"][i]) - np.array([0,self.g])
+        #         sum_x += sf["m"][j]*vij*cubic_spline_2d(xij,fluid["h"][i])
 
-                sum_rho += sf["m"][j]*np.dot(vij,cubic_spline_2d(xij,fluid["h"][i]))/sf["rho"][j]
-                sum_vel += sf["m"][j]*(pi_rhoi+pj_rhoj+pi_ij)*cubic_spline_derivative_2d(xij,fluid["h"][i]) - np.array([0,self.g])
-                sum_x += sf["m"][j]*vij*cubic_spline_2d(xij,fluid["h"][i])
+        #     arho_fluid[i] = fluid["rho"][i] * sum_rho
+        #     acc_fluid[i] = -1 * sum_vel
+        #     dxdt_fluid[i] = fluid["v"][i] + self.epsilon * sum_x
 
-            arho_fluid[i] = fluid["rho"][i] * sum_rho
-            acc_fluid[i] = -1 * sum_vel
-            dxdt_fluid[i] = fluid["v"][i] + self.epsilon * sum_x
-
-        # arho_fluid, acc_fluid, dxdt_fluid = self.fluid_calc(fluid, sf)
+        arho_fluid, acc_fluid, dxdt_fluid = self.fluid_calc(self.fluid, self.sf)
 
         self.fluid["rho"] += arho_fluid * dt
         self.fluid["v"] += acc_fluid * dt
         self.fluid["x"] += dxdt_fluid * dt
-        self.fluid["p"] = B*((fluid["rho"]/self.rho0)**(self.gamma) - 1)
+        self.fluid["p"] = B*((self.fluid["rho"]/self.rho0)**(self.gamma) - 1)
 
         self.sf["p"] = np.append(self.solid["p"],self.fluid["p"])
         self.sf["rho"] = np.append(self.solid["rho"],self.fluid["rho"])
