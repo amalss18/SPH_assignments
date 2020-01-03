c6d396559eb66f3ff4c1561752b756091c017c5b
amalss18 <amalssebastian@gmail.com>
2020-01-03 11:06:54 +0530

    shocktube results

--- assignment_3.py
+++ assignment_3.py
@@ -38,16 +38,6 @@ def EOS(e,rho):
     p = (gamma-1)*rho*e
     return p
 
-def density_calc(m,x,h):
-    rho = []
-    for i in range(len(x)):
-        sum = 0
-        for j in range(len(x)):
-            xij = x[i]-x[j]
-            sum += m[j]*(cubic_spline(xij,h[j])+cubic_spline(xij,h[i]))*0.5
-        rho.append(sum)
-    return rho
-
 @njit(parallel=True)
 def integrate(intial,dt,r_change):
     final = intial+dt*r_change
@@ -70,7 +60,7 @@ def continuity_eqaution(rho,m,v,h,x,dt):
 
 @njit(parallel=True)
 def artificial_viscosity(xab,vab,rhoab,cab,hab,eta):
-    if vab*xab<0:
+    if vab*xab<=0:
         muab = hab*vab*xab/(abs(xab)**2+eta**2)
 
         piab = (-cab*muab + muab**2)/rhoab
@@ -116,7 +106,7 @@ def energy_equation(rho,m,x,p,v,c,h,dt,e,eta):
             hab = (h[a]+h[b])*0.5
 
             sum+=(m[b])*((p[a]/rho[a]**2)+(p[b]/rho[b]**2)+artificial_viscosity(xab,vab,rhoab,cab,hab,eta))* \
-                (cubic_spline_derivative(xab,h[a])+cubic_spline_derivative(xab,h[b]))*0.5
+                (cubic_spline_derivative(xab,h[a])+cubic_spline_derivative(xab,h[b]))*0.5*vab
 
         ae[a] = 0.5*sum
 
@@ -140,11 +130,11 @@ def intialize(x1,x2,dx1,dx2):
     p = np.concatenate((p1,p2))
 
     v1 = np.zeros_like(x1)
-    v2 = np.zeros_like(x2)*0
+    v2 = np.zeros_like(x2)
     v = np.concatenate((v1,v2))
 
-    h1 = np.ones_like(x1)*dx1
-    h2 = np.ones_like(x2)*dx2
+    h1 = 2*np.ones_like(x1)*dx1
+    h2 = 2*np.ones_like(x2)*dx2
     h = np.concatenate((h1,h2))
 
     e = p/(rho*0.4)
@@ -192,7 +182,7 @@ if __name__ == "__main__":
         v_hist[i+1], x_hist[i+1] = momentum_equation(rho_hist[i],m,x_hist[i],p_hist[i],v_hist[i],c,h,dt,eta)
         e_hist[i+1] = energy_equation(rho_hist[i],m,x_hist[i],p_hist[i],v_hist[i],c,h,dt,e_hist[i],eta)
 
-plt.plot(rho_hist[-1],x_hist[-1])
+plt.plot(x_hist[-1],rho_hist[-1])
 plt.show()
 
 
